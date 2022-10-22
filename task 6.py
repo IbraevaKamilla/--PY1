4c674fa2b5b4f6f149f1b266293fb916093c8180
IbraevaKamilla <115577093+IbraevaKamilla@users.noreply.github.com>
2022-10-22 20:50:21 +0300

    Update task 6.py
    Я перерешала задание №6, но мой ответ все еще не сходится с эталоном. Формулировка задания о первом максимальном и последнем элементе и множественном присвоении для них мне не до конца ясна. В моем коде последний элемент - это положительное число с наименьшим значением. Но это можно также интерпретировать таким образом: последний элемент - наименьший из всех чисел в списке, не важно, отрицательных или положительных. Заранее спасибо за внимание к моему комментарию и обратную связь.

--- task 6.py
+++ task 6.py
@@ -1,28 +1,20 @@
+# ПЕРЕДЕЛАННОЕ ЗАДАНИЕ 
 list_numbers = [2, -93, -2, 8, -36, -44, -1, -85, -14, 90, -22, -90, -100, -8, 38, -92, -45, 67, 53, 25]
-max_index = 0
-max_number = list_numbers[max_index]
-
-for i, current_number in enumerate (list_numbers):
-    max_number = list_numbers[max_index]
-    if current_number > max_number:
-        max_index = i
-        max_number = list_numbers[max_index]
-    print (max_number, max_index)
-
-min_index = 0
-min_number = list_numbers [min_index]
-for i, current_number in enumerate (list_numbers):
-    min_number = list_numbers [min_index]
-    if current_number < min_number:
-        min_index = i
-        min_number = list_numbers [min_index]
-    print(min_number, min_index)
-
-max_number, min_number = min_number, max_number
-print(min_number, max_number)
-print(list_numbers)
-
-max_number, min_number = "max_number", "min_number"
-max_number, min_number = min_number, max_number
-print(max_number, min_number)
-
+list_for_sort =  [2, -93, -2, 8, -36, -44, -1, -85, -14, 90, -22, -90, -100, -8, 38, -92, -45, 67, 53, 25]
+# новый список для сортировки
+list_for_sort.sort() #сортировка списка по возрастанию
+print (list_for_sort)
+# [-100, -93, -92, -90, -85, -45, -44, -36, -22, -14, -8, -2, -1, 2, 8, 25, 38, 53, 67, 90]
+# максимальный элемент списка это 90. его индекс равен -1
+min(i for i in list_numbers if i > 0) #поиск минимального положительного элемента
+print(min)
+print(list_for_sort.index(2))
+# 13
+print(list_for_sort.index(90))
+# -1
+print(list_numbers.index(2))
+# 0
+print(list_numbers.index(90))
+# 9
+list_numbers [9], list_numbers [0] = list_numbers [0], list_numbers [9]
+print (list_numbers)
